a54e7efc6ab13de2d678028b46d5ffb311bdbc09
Jonatan <jonata@vivver.com.br>
2024-07-09 15:40:27 -0300

    gerando contatos

--- contact/admin.py
+++ contact/admin.py
@@ -4,10 +4,18 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'category',
     ordering = '-id', 
-    list_filter = 'created_date',
+    list_filter = 'category',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 250
     list_display_links = 'id', 'first_name',
+
+
+
+@admin.register(models.Category)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id', 
+    
--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
 
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,6 +20,15 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
-
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%M/')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, blank=True, null=True
+        )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, blank=True, null=True
+        )
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
