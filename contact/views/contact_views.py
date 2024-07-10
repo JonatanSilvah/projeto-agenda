532fc82156d8ad1ae0601a8c0d43cafc6698ef64
Jonatan <jonata@vivver.com.br>
2024-07-09 22:56:29 -0300

    montando tabela

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from contact.models import Contact
+
 
 def index(request):
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context
     )
